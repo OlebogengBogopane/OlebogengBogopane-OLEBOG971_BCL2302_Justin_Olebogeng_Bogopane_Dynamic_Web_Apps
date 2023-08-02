ff1d636a522998913b2b5af932d182cc9d4155fc
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-08-02 13:33:11 +0200

    components update

--- PodcastApp/PodProject/ThePodcastCorner/src/App.jsx
+++ PodcastApp/PodProject/ThePodcastCorner/src/App.jsx
@@ -3,14 +3,12 @@ import React, { useState, useEffect } from 'react';
 import "./App.css";
 import Navbar from './Navbar';
 import {Link} from 'react-router-dom'
-import Sort from './components/Sort'
 
 const Main = () => {
   const [podcasts, setPodcasts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [selectedSeason, setSelectedSeason] = useState(null);
   const [selectedPodcast, setSelectedPodcast] = useState(null)
-  const [sortBy,setSortBy] = useState('default')
 
   useEffect(() => {
 
@@ -38,46 +36,21 @@ const Main = () => {
   const clearSelectedPodcast = () => {
     setSelectedPodcast(null);
   };
-  const handleSearchChange =(event) => {
-    setSearchQuery(event.target.value)
-  };
-  const handleSortChange = (criteria) => {
-    setSortBy(criteria);
-    let sortedShows = [
-      ...ShowPreviews];
-      switch(criteria){
-      case 'titleAZ':
-    sortedShows.sort((a,b)=> a.title.localeCompare(b.title));
-    break  
-      case 'titleZA':
-    sortedShows.sort((a,b)=> b.title.localeCompare(a.title));
-    break  
-      case 'DateUpdatedAscending':
-    sortedShows.sort((a,b)=> new Date(a.updated)- newDate(b.updated))
-    break;  
-      case 'DateUpdatedDescending':
-    sortedShows.sort((a,b)=> new Date(a.updated)- newDate(b.updated))
-    break;  
-    default:
-      break;
-    }
-    
-  }
 
 
 
   return (
     <>
       <Navbar />
-      <Sort />
+      <sort />
 
       {selectedPodcast && (
         <div className="selected-podcast-details">
           <img src={selectedPodcast.image} alt={selectedPodcast.title} />
           <h2>Title:{selectedPodcast.title}</h2>
-          <p>Id:{selectedPodcast.id}</p>
-          <p>Updated:{selectedPodcast.updated}</p>
-          <p>Season :{selectedPodcast.seasons}</p>
+          <p>Genre:{selectedPodcast.genre}</p>
+          <p>Episode:{selectedPodcast.episode}</p>
+          <p>Season :{selectedPodcast.season}</p>
           <p>Description:{selectedPodcast.description}</p>
 
           <button onClick={clearSelectedPodcast}>Close</button>
---
+++ PodcastApp/PodProject/ThePodcastCorner/src/components/Discover.jsx
@@ -0,0 +1,8 @@
+import {genres  } from 'react'
+import {Loader,Podcast  } from '../components'
+
+
+
+const Discover = () => <div> Discover </div>
+  
+export default Discover;
